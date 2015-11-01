2a9a9e4318f66408e3c77391c95a77aac5523980
linxuping <linxuping@126.com>
2015-11-01 13:17:47 +0800

    send notify to mobile

--- app/src/main/java/com/app/MainActivity.java
+++ app/src/main/java/com/app/MainActivity.java
@@ -2,12 +2,18 @@ package com.app;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.TextView;
@@ -29,10 +35,34 @@ public class MainActivity extends Activity {
 	private TextView msgView = null;
     Vibrator m_vib = null;
 
+    private void sendNotify(){
+        //定义NotificationManager
+        String ns = Context.NOTIFICATION_SERVICE;
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(ns);
+        //定义通知栏展现的内容信息
+        int icon = R.drawable.icon;
+        CharSequence tickerText = "我的通知栏标题";
+        long when = System.currentTimeMillis();
+        Notification notification = new Notification(icon, tickerText, when);
+
+        //定义下拉通知栏时要展现的内容信息
+        Context context = getApplicationContext();
+        CharSequence contentTitle = "我的通知栏标展开标题";
+        CharSequence contentText = "我的通知栏展开详细内容";
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                notificationIntent, 0);
+        notification.setLatestEventInfo(context, contentTitle, contentText,
+                contentIntent);
+
+        //用mNotificationManager的notify方法通知用户生成标题栏消息通知
+        mNotificationManager.notify(1, notification);
+    }
 	@SuppressLint("SetJavaScriptEnabled")
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.main);
 		contentWebView = (WebView) findViewById(R.id.webview);
 		//msgView = (TextView) findViewById(R.id.msg);
@@ -82,7 +112,8 @@ public class MainActivity extends Activity {
                     @Override
                     public void onStringAvailable(String s) {
                         //if ("Hello Server".equals(s))
-                        m_vib.vibrate(2000);
+                        m_vib.vibrate(1000);
+                        sendNotify();
                         webSocket.send("Welcome Client!");
                     }
                 });
